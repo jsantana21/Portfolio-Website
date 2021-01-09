27d5468dfc7e5fac9e59e2c68b871381c3ac6e0a
Juan Santana <juansantanajunior@gmail.com>
2021-01-09 15:55:07 -0500

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is ',
   name: 'Juan Santana',
-  subtitle: 'An Aspiring Software Engineer',
+  subtitle: 'A Software Engineer',
   cta: 'Get to know me',
 };
 
